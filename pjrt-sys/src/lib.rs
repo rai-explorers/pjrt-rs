56b7de1f8c29d7bc7a6b5b9ddce8bfa038a84838
cksac <cs.cksac@gmail.com>
2024-09-20 15:43:32 +0800

    fix clippy warning

--- pjrt-sys/src/lib.rs
+++ pjrt-sys/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+#![allow(clippy::too_long_first_doc_paragraph)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
