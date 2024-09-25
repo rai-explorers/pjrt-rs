618a41d48d9545dba8c8ec6bc3cf6447eef7a39c
cksac <cs.cksac@gmail.com>
2024-09-25 16:03:03 +0800

    fix examples

--- pjrt/examples/basic_bf16.rs
+++ pjrt/examples/basic_bf16.rs
@@ -17,12 +17,12 @@ fn main() -> Result<()> {
     let a = HostBuffer::from_scalar(half::bf16::from_f32(1.25));
     println!("input = {:?}", a);
 
-    let inputs = a.copy_to_sync(&client).call()?;
+    let inputs = a.to_sync(&client).copy()?;
 
     let result = loaded_executable.execution(inputs).run_sync()?;
 
     let ouput = &result[0][0];
-    let output = ouput.copy_to_host_sync()?;
+    let output = ouput.to_host_sync().copy()?;
     println!("output= {:?}", output);
 
     Ok(())
