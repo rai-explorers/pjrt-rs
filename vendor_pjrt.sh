cf3fdd73b1f008a40d8b025f2a092b25a00a1972
cksac <cs.cksac@gmail.com>
2025-08-27 23:46:16 +0800

    update bazel version

--- vendor_pjrt.sh
+++ vendor_pjrt.sh
@@ -1,5 +1,8 @@
 cd third_party
 bazel build //:pjrt_headers //:pjrt_protos
 
-mv bazel-bin/include ../pjrt-sys/include
-mv bazel-bin/protos ../pjrt-sys/protos
+rm -rf ../pjrt-sys/include/*
+rm -rf ../pjrt-sys/protos/*
+
+tar -xvf bazel-bin/pjrt_headers.tar -C ../pjrt-sys/include
+tar -xvf bazel-bin/pjrt_protos.tar -C ../pjrt-sys/protos
