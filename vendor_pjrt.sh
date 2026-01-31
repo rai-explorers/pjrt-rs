5448e162c63265bbb11f73ff1093d7efe25de73a
cksac <cs.cksac@gmail.com>
2026-01-31 21:23:53 +0800

    update vendor prjt

--- vendor_pjrt.sh
+++ vendor_pjrt.sh
@@ -4,6 +4,6 @@ bazelisk build //:pjrt_headers //:pjrt_protos
 rm -rf ../pjrt-sys/include/*
 rm -rf ../pjrt-sys/protos/*
 
-tar -xf bazel-bin/pjrt_headers.tar -C ../pjrt-sys/include
+tar -xf bazel-bin/pjrt_include.tar -C ../pjrt-sys/include
 tar -xf bazel-bin/pjrt_protos.tar -C ../pjrt-sys/protos
 cd ..
